156a67cce4cec02e5a349915fa5d2b861f588a8f
neos <diegoaliagalahuerta@gmail.com>
2020-12-10 11:02:54 +0000

    a

--- index.js
+++ index.js
@@ -10,7 +10,31 @@ bot.on('text', msg => {
   if (lastmsg == "uwu") {
     msg.reply.text("owo");
   }
-  
+  if (lastmsg == "A") {
+    msg.reply.text("A");
+  }
+  if (lastmsg == "ª") {
+    msg.reply.text("ª");
+  }
+  if (lastmsg === 'mesco lindo') {
+    msg.reply.text('posí');
+  }
+  if (lastmsg === 'bigu manda') {
+    msg.reply.text('a ver tecnicamente si');
+  }
+  if (lastmsg === 'NeOS ataca!') {
+    msg.reply.text('MAO');
+  }
+  if (lastmsg === "/help") {
+    msg.reply.text("no help for u :,<")
+  }
+  if (lastmsg.indexOf("/math ")==0) {
+    var result;
+    var operation=lastmsg.slice(6);
+    try {result=eval(operation);}
+    catch(err) {msg.reply.text("error lmao");}
+    msg.reply.text(result);
+  }
 });
 
 bot.start();
